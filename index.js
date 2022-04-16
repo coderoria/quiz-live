92d69748805c624f706217c823a61b8a24bfe4cd
Noah Groß <me@codesalat.dev>
2022-04-17 00:26:11 +0200

    Start auth integration

--- index.js
+++ index.js
@@ -1,10 +1,45 @@
 import express from "express";
 const app = express();
+import http from "http";
+const server = http.createServer(app);
 import sqlite3 from "sqlite3";
 import { open } from "sqlite";
+import { join, dirname } from "path";
+import { fileURLToPath } from "url";
+import { Server } from "socket.io";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import crypto from "crypto";
+const io = new Server(server);
 
-app.get("/", (req, res) => {
-  res.send("Not the text!");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+
+app.get("/auth", (req, res) => {
+  res.sendFile(dirname(fileURLToPath(import.meta.url)) + "/webapp/auth.html");
+});
+
+app.post("/auth", (req, res) => {
+  if (req.body.token == "auth") {
+    res.redirect("/");
+  } else {
+    res.redirect("?wrong=1");
+  }
+});
+
+app.use(
+  express.static(join(dirname(fileURLToPath(import.meta.url)), "/webapp"))
+);
+
+app.use((req, res, next) => {
+  if (req.cookies.token == null) {
+    res.redirect("/auth");
+  }
+});
+
+io.on("connection", (socket) => {
+  console.log("Socket " + socket.id + " connected!");
 });
 
 const db = await open({
@@ -16,4 +51,15 @@ await db.migrate();
 
 console.log("Migrated database to newest schema");
 
-app.listen(3000);
+db.get("SELECT token FROM token;").then((data) => {
+  if (data == null) {
+    crypto.randomBytes(32, async (err, buff) => {
+      console.log(`Generated token: ${buff.toString("base64")}`);
+      await db.run("INSERT INTO token VALUES (?);", buff.toString("base64"));
+    });
+  }
+});
+
+server.listen(3000, () => {
+  console.log("Listening on *:3000");
+});
